1034e85cfbb5f9aff6174db53bf7d38904dc3888
Juan Antamba <juan.anatamba@udla.edu.ec>
2023-10-14 23:18:20 -0500

    Creacion de bucle principal , se verifica cada numero mediante un porcentaje para saber si es multiplo de 5, y si lo es se lo agrega al contador

--- Ejercicio 13.c
+++ Ejercicio 13.c
@@ -5,10 +5,16 @@ int main() {
 
     printf("Ingrese un numero entero: ");
     scanf("%d", &numero);
+
     if (numero < 1) {
         printf("El numero ingresado debe ser mayor o igual a 1.\n");
     } else {
         printf("Multiplos de 5 comprendidos entre 1 y %d:\n", numero);
 
+        for (int multiploDeCinco = 1; multiploDeCinco <= numero; multiploDeCinco=multiploDeCinco+1) {
+            if (multiploDeCinco % 5 == 0) {
+                printf("%d ", multiploDeCinco);
+            }
+        }
     }
 }
