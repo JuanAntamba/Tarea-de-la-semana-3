9384d789a416313f8561aa58308ce21460f2ca23
Juan Antamba <juan.antamba@udla.edu.ec>
2023-10-14 20:19:37 -0500

    Iniciacion variable num y operaciones nesesarias para conseguir los numeros deseados

--- Ejercicio 49.c
+++ Ejercicio 49.c
@@ -3,6 +3,9 @@
 int main() {
     for (int x = 1; x <= 3; x=x+1) {
         for (int y = 1; y <= 3; y=y+1) {
+            int num = (x - 1) * 3 + y;
+            printf("%d %d %d\n", num, x, y);
         }
     }
+    return 0;
 }
