2825b1f39c21bc29a43c888e23784e325224e0c8
Juan Antamba <juan.antamba@udla.edu.ec>
2023-10-14 21:20:50 -0500

    Inicializacion de variables, y impresion de la premisa del problema para el usuario

---
+++ Ejercicio 31.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int numero, suma = 0, contador = 0;
+
+    printf("Ingrese numeros de su eleccion, recordando que para el promedio general se tomara en cuenta numeros terminados en 5. Ingrese 0 para terminar.\n");
+
+}
