b22f4d6df13e588ce48201463dd6e2889dbefcae
Juan Antamba <juan.antamba@udla.edu.ec>
2023-10-14 21:28:17 -0500

    Inicia un Do , se comprende las premisas del enunciado entendiendo sus restriciones

--- Ejercicio 31.c
+++ Ejercicio 31.c
@@ -5,4 +5,18 @@ int main() {
 
     printf("Ingrese numeros de su eleccion, recordando que para el promedio general se tomara en cuenta numeros terminados en 5. Ingrese 0 para terminar.\n");
 
+    do {
+        printf("Ingrese un numero: ");
+        scanf("%d", &numero);
+
+        if (numero == 0) {
+            break; 
+        }
+
+        if (numero % 10 == 5) {
+            suma = suma + numero;
+            contador=contador+1;
+        }
+    }
 }
+
