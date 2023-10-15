3100454b1e9b01bdea091299f3d9ccd6d875e85c
Juan Antamba <juan.anatamba@udla.edu.ec>
2023-10-14 22:54:03 -0500

    Creacion restriccion(numeros ayores a 1), si no se imprime mensaje de multilplos de 5

--- Ejercicio 13.c
+++ Ejercicio 13.c
@@ -5,5 +5,10 @@ int main() {
 
     printf("Ingrese un numero entero: ");
     scanf("%d", &numero);
+    if (numero < 1) {
+        printf("El numero ingresado debe ser mayor o igual a 1.\n");
+    } else {
+        printf("Multiplos de 5 comprendidos entre 1 y %d:\n", numero);
 
+    }
 }
