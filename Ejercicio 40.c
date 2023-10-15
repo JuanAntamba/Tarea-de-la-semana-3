e076208e7a8699b7229dab352458638ffd94d45e
Juan Antamba <juan.antamba@udla.edu.ec>
2023-10-14 20:48:52 -0500

    Primer if, y primera restriccion

--- Ejercicio 40.c
+++ Ejercicio 40.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int numero, primerNum = 0, segundoNum = 1, siguienteNum;
+    int numero, primero = 0, segundo = 1, siguiente;
 
     printf("Ingrese el numero de dos digitos: ");
     scanf("%d", &numero);
+
+    if (numero < 10 || numero > 99) {
+        printf("El numero ingresado solo puede tener dos digitos.\n");
+    } else {
+        int esFibonacci = 0;  
+
+    }
 }
